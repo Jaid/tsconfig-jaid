3f6747c08701e79792c0dca71a47093baac7e267
Jaid <jaid.jsx@gmail.com>
2020-08-24 13:52:07 +0200

    Removed pipeline operator
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,11 +1,10 @@
-import path from "path"
-
 import browserslist from "browserslist"
+import path from "path"
 
 /**
  * @type { import("../src") }
  */
-const browserslistConfig = (process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src")) |> require
+const browserslistConfig = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
 
 it("Should return a proper version list for given browserslist query", () => {
   const result = browserslist(browserslistConfig)
