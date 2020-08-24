3dad55946feab9832118e1518a30d6b4b3bc069a
Jaid <jaid.jsx@gmail.com>
2020-08-24 17:20:29 +0200

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ import path from "path"
 /**
  * @type { import("../src") }
  */
-const typescriptConfig = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
+const tsconfigJaid = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
 
 it("Should return a proper TypeScript configuration", () => {
-  expect(typescriptConfig.compilerOptions.newLine).toStrictEqual("lf")
+  expect(tsconfigJaid().compilerOptions.newLine).toStrictEqual("lf")
 })
