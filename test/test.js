456d55709c58bbb13dc45b291221c9a26e10ab9a
Jaid <jaid.jsx@gmail.com>
2022-06-14 21:17:46 +0200

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -1,9 +1,13 @@
 import path from "node:path"
+import {fileURLToPath, pathToFileURL} from "node:url"
+
+const dirName = path.dirname(fileURLToPath(import.meta.url))
+const indexPath = process.env.MAIN ?? path.join(dirName, "..", "src", "index.js")
 
 /**
  * @type { import("../src") }
  */
-const tsconfigJaid = require(process.env.MAIN ? path.resolve(process.env.MAIN) : path.join(__dirname, "..", "src"))
+const {default: tsconfigJaid} = await import(pathToFileURL(indexPath))
 
 it("Should return a proper TypeScript configuration", () => {
   expect(tsconfigJaid().compilerOptions.newLine).toStrictEqual("lf")
