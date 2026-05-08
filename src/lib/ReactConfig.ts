81ee598b421b575206e5d897c701112ab83ceca7
Jaid <6216144+Jaid@users.noreply.github.com>
2026-05-08 17:19:36 +0100

    Added lib “dom.iterable” to ReactConfig
    Signed-off-by: Jaid <6216144+Jaid@users.noreply.github.com>

--- src/lib/ReactConfig.ts
+++ src/lib/ReactConfig.ts
@@ -4,6 +4,7 @@ export class ReactConfig extends GenericConfig {
   constructor() {
     super()
     this.addLib('dom')
+    this.addLib('dom.iterable')
     this.addLib('webworker')
     if (!this.tsconfig.compilerOptions) {
       this.tsconfig.compilerOptions = {}
